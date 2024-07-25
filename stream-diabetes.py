98e3808dc8924bdb1eeb75b3625bd4ce36711521
Allun Samudra Harum <allunsamudraa@gmail.com>
2024-07-25 14:50:41 +0700

    Update stream-diabetes.py

--- stream-diabetes.py
+++ stream-diabetes.py
@@ -10,24 +10,30 @@ st.title('Prediksi Diabetes')
 # Add a subtitle for better understanding
 st.subheader('Masukkan Data untuk Memprediksi Diabetes')
 
-# Two row layout with four columns each
-col1, col2, col3, col4 = st.columns(4)
+# Using wider columns
+col1, col2 = st.columns([1, 1])
 
 with col1:
     Pregnancies = st.number_input('Angka Kehamilan', min_value=0, step=1, format="%d")
 with col2:
     Glucose = st.number_input('Glukosa', min_value=0, step=1, format="%d")
+    
+col3, col4 = st.columns([1, 1])
+    
 with col3:
     BloodPressure = st.number_input('Tekanan Darah (mmHg)', min_value=0, step=1, format="%d")
 with col4:
     SkinThickness = st.number_input('Ketebalan lipatan kulit trisep (mm)', min_value=0, step=1, format="%d")
 
-col5, col6, col7, col8 = st.columns(4)
+col5, col6 = st.columns([1, 1])
 
 with col5:
     Insulin = st.number_input('Insulin serum (mu U/ml)', min_value=0, step=1, format="%d")
 with col6:
     BMI = st.number_input('BMI', min_value=0.0, step=0.1, format="%.1f")
+
+col7, col8 = st.columns([1, 1])
+
 with col7:
     DiabetesPedigreeFunction = st.number_input('Indikator riwayat diabetes dalam keluarga', min_value=0.0, step=0.01, format="%.2f")
 with col8:
