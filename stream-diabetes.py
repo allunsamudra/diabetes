f824026d051ed84f8f1fbf72ef6461b5e6429d1d
Allun Samudra Harum <allunsamudraa@gmail.com>
2024-07-25 14:44:05 +0700

    Update stream-diabetes.py

--- stream-diabetes.py
+++ stream-diabetes.py
@@ -14,28 +14,28 @@ st.subheader('Masukkan Data untuk Memprediksi Diabetes')
 col1, col2, col3, col4 = st.columns(4)
 
 with col1:
-    Pregnancies = st.number_input('Bulan Kehamilan', min_value=0, step=1, format="%d")
+    Pregnancies = st.number_input('Angka Kehamilan', min_value=0, step=1, format="%d")
 
 with col2:
     Glucose = st.number_input('Glukosa', min_value=0, step=1, format="%d")
 
 with col3:
-    BloodPressure = st.number_input('Tekanan Darah', min_value=0, step=1, format="%d")
+    BloodPressure = st.number_input('Tekanan Darah (mmHg)', min_value=0, step=1, format="%d")
 
 with col4:
-    SkinThickness = st.number_input('Ketebalan Kulit', min_value=0, step=1, format="%d")
+    SkinThickness = st.number_input('Ketebalan lipatan kulit trisep (mm)', min_value=0, step=1, format="%d")
 
 with col1:
-    Insulin = st.number_input('Insulin', min_value=0, step=1, format="%d")
+    Insulin = st.number_input('Insulin serum (mu U/ml)', min_value=0, step=1, format="%d")
 
 with col2:
     BMI = st.number_input('BMI', min_value=0.0, step=0.1, format="%.1f")
 
 with col3:
-    DiabetesPedigreeFunction = st.number_input('Diabetes Pedigree Function', min_value=0.0, step=0.01, format="%.2f")
+    DiabetesPedigreeFunction = st.number_input('Indikator riwayat diabetes dalam keluarga', min_value=0.0, step=0.01, format="%.2f")
 
 with col4:
-    Age = st.number_input('Umur', min_value=0, step=1, format="%d")
+    Age = st.number_input('Usia (tahun)', min_value=0, step=1, format="%d")
 
 # Placeholder for the diagnosis result
 diab_diagnosis = ''
